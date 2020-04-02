97fc0877cccd6956b49e50ed2576755d63ba09e2
theclassicwizard <srkjadara@gmail.com>
2020-04-02 17:55:55 +0530

    new

--- scripts/master.js
+++ scripts/master.js
@@ -3,3 +3,9 @@ $(function () {
 })
 
 $('body').scrollspy({ target: '#spyData' })
+
+$(document).ready(function(){
+    $("ul.nav li a[href^='#']").click(function(){
+        $("html, body").stop().animate({scrollTop: $($(this).attr("href")).offset().top}, 400);
+    });
+});
