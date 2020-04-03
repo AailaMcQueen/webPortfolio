c0ea901753ede6f09a8d85e6eeaa220d62194038
theclassicwizard <srkjadara@gmail.com>
2020-04-03 17:10:03 +0530

    Preloader

--- scripts/master.js
+++ scripts/master.js
@@ -14,3 +14,11 @@ $('.navbar-collapse a:not(.dropdown-toggle)').click(function(){
     if($(window).width() < 990 )
         $('.navbar-collapse').collapse('hide');
 });
+
+$(window).on('load', function() { // makes sure the whole site is loaded
+  $('#logocontainer').fadeOut();
+  $('#loaderLG').delay(770).css({'overflow':'hidden'});
+  $('#loaderLG').delay(770).css({'visibility':'hidden'});
+  $('.bodyPart').delay(770).css({'overflow':'visible'});
+  $('.bodyPart').delay(770).css({'visibility':'visible'});
+})
